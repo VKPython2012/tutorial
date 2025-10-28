405f41216474e316f75632760b1d89d2f9427165
VKPython2012 <vinhkhoi1212@gmail.com>
2025-10-28 18:46:52 +0700

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://guileless-pasca-1cd7d0.netlify.app/",
+  integrations: [preact()]
+});
---
+++ src/pages/astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://guileless-pasca-1cd7d0.netlify.app/",
+  integrations: [preact()],
+});
